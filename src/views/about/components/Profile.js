3c546944624f6bb6e23a18b411fab2c4c852e8f2
susanwen <susan.wen.wjy@gmail.com>
2022-07-22 01:01:14 -0400

    Create skeleton to convert to React

--- src/views/about/components/Profile.js
+++ src/views/about/components/Profile.js
@@ -3,6 +3,8 @@ import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Profile = (props) => {
 
@@ -17,6 +19,10 @@ const Profile = (props) => {
                         <h2>Hi! I'm Susan Wen</h2>
                         I'm a software engineer and indie game developer, avid lover of storytelling through technology and arts alike.
                     </Typography>
+
+                    <GitHubIcon/>
+                    <LinkedInIcon/>
+                    
                 </Grid>
             </Grid>
         </Box>
